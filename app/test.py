a50fc75c47012434ee707eeab4757581597e04f3
Arii Keisuke <ariikeisuke@gmail.com>
2021-11-10 20:16:06 +0900

    ファイル分け完了

--- app/test.py
+++ app/test.py
@@ -1,77 +1,19 @@
 
-# school_name = "有井教室"
-# station = "岩本町"
-# address = "千代田区岩本町"
+from test2 import school_master2
 
-# sql3 = "insert into school_master(school_name,"+\
-#                                 "station,"+\
-#                                 "address) "+\
-#                                 "values(school_name = '%s',"  % (school_name)+\
-#                                 "sation = '%s')" % (station)
-# name = "a"
+print(school_master2)
 
-# sql = "SELECT * FROM hogehoge WHERE name = '%s'" % (name,)
-
-# sql2 = (school_name, station)
-# print(sql3)
-
-# from flask import Flask,render_template,request
-# import psycopg2
-
-# +++
-
-import psycopg2
-
-path = "localhost"
-port = "5432"
-dbname = "testdb"
-user = "ariikeisuke"
-password = "candy1225"
-conText = "host={} port={} dbname={} user={} password={}"
-conText = conText.format(path,port,dbname,user,password)
-connection = psycopg2.connect(conText)
-cur = connection.cursor()
-
-
-unique_school_id = "30"
-school_name = 'kei'
-nearest_station = "岩本町駅"
-address = "千代田区岩本町"
-operating_hour = "20"
-lesson_hour = "30"
-distance_station = "5"
-number_students = "100"
-lesson_time = "100"
-numbers_of_lesson = "111"
-price_minutes = "110"
-price_month = "112"
-
-
-sql = "insert into school_master(unique_school_id,"+\
-                                    "school_name,"+\
-                                    "nearest_station,"+\
-                                    "address,"+\
-                                    "operating_hour,"+\
-                                    "lesson_hour,"+\
-                                    "distance_station,"+\
-                                    "number_students,"+\
-                                    "lesson_time,"+\
-                                    "numbers_of_lesson,"+\
-                                    "price_minutes,"+\
-                                    "price_month) "+\
-                                    "values(%s," % (unique_school_id)+\
-                                    "'%s'," % (school_name)+\
-                                    "'%s'," % (nearest_station)+\
-                                    "'%s'," % (address)+\
-                                    "%s," % (operating_hour)+\
-                                    "%s," % (lesson_hour)+\
-                                    "%s," % (distance_station)+\
-                                    "%s," % (number_students)+\
-                                    "%s," % (lesson_time)+\
-                                    "%s," % (numbers_of_lesson)+\
-                                    "%s," % (price_minutes)+\
-                                    "%s);" % (price_month)
-
-
-cur.execute(sql)
-connection.commit()
+# insert_table = school_master2(
+#     school_name = 1,
+#     nearest_station = 2,
+#     address = 3,
+#     operating_hour = 4,
+#     lesson_hour = 5,
+#     distance_station = 5,
+#     number_students = 5,
+#     lesson_time = 6,
+#     numbers_of_lesson = 5,
+#     price_minutes = 6,
+#     price_month = 7)
+    
+# print(insert_table)
